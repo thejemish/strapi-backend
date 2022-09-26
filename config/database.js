b1e6d936651f2360bf1e0dcb8c3b6a435bf620da
jemish <jemishmalaviya35@gmail.com>
2022-09-26 12:39:39 +0530

    bug fix

--- config/database.js
+++ config/database.js
@@ -2,12 +2,12 @@ module.exports = ({ env }) => ({
   connection: {
     client: "mysql",
     connection: {
-      host: env("DATABASE_HOST", "127.0.0.1"),
+      host: env("DATABASE_HOST", "82.180.143.103"),
       port: env.int("DATABASE_PORT", 3306),
-      database: env("DATABASE_NAME", "pod"),
-      user: env("DATABASE_USERNAME", "root"),
-      password: env("DATABASE_PASSWORD", ""),
-      ssl: env.bool("DATABASE_SSL", false),
+      database: env("DATABASE_NAME", "u976353708_podadium"),
+      user: env("DATABASE_USERNAME", "u976353708_pod"),
+      password: env("DATABASE_PASSWORD", "YQ19$rn6Lu%4X$"),
+      ssl: env.bool("DATABASE_SSL", true),
     },
   },
 });
